d64d5ad926243f437cd1aff6f971102265c2aa95
stefano basile <sbasile@companieshouse.gov.uk>
2025-02-03 22:52:04 +0000

    fix vpc id

--- terraform/groups/lambda/lambda.tf
+++ terraform/groups/lambda/lambda.tf
@@ -127,7 +127,7 @@ resource "aws_lambda_permission" "allow_eventbridge" {
 
 // Add a VPC endpoint to allow the Lambda function to access ECS
 resource "aws_vpc_endpoint" "ecs" {
-  vpc_id       = data.aws_vpc.vpc_id
+  vpc_id       = data.aws_vpc.vpc.id
   service_name = "com.amazonaws.${var.aws_region}.ecs"
   subnet_ids   = local.application_subnet_ids
   security_group_ids = [aws_security_group.services_dashboard_lambda_sg.id]

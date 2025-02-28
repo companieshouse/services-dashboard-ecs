d68aec1ca315bcdf0be5838d7524dafbe86825dd
stefano basile <sbasile@companieshouse.gov.uk>
2025-02-28 15:52:57 +0000

    add a debug node test-ecs-connectyivity function

--- terraform/groups/lambda/lambda.tf
+++ terraform/groups/lambda/lambda.tf
@@ -63,6 +63,24 @@ resource "aws_iam_role_policy_attachment" "ecs_operations_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
 }
 
+resource "aws_lambda_function" "node_test" {
+  depends_on = [aws_cloudwatch_log_group.lambda_log_group, aws_vpc_endpoint.ecs]
+
+  function_name = "node_test_ecs"
+  role          = aws_iam_role.lambda_execution_role.arn
+
+  s3_bucket = var.release_bucket_name
+  s3_key    = "services-dashboard-ecs/node_test_ecs.1.zip"
+
+  handler = "node_test_ecs.lambdaHandler"
+  runtime = "nodejs18.x"
+  memory_size     = 128
+  timeout         = 10
+  vpc_config {
+    subnet_ids         = local.application_subnet_ids
+    security_group_ids = [aws_security_group.services_dashboard_lambda_sg.id]
+  }
+}
 resource "aws_lambda_function" "python_test" {
   depends_on = [aws_cloudwatch_log_group.lambda_log_group, aws_vpc_endpoint.ecs]
 

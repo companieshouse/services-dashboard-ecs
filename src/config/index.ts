7486fbfc4ff7813f515fc9848da36fd7c5889094
sbasile-ch <sbasile@companieshouse.gov.uk>
2025-09-03 08:06:57 +0100

    remove config var

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,6 @@ export const APPLICATION_NAME= "services-dashboard-ecs";
 export const MONGO_PROTOCOL = getEnvironmentValue("MONGO_PROTOCOL", "mongodb");
 export const MONGO_USER     = getEnvironmentValue("MONGO_USER");
 export const MONGO_PASSWORD_PARAMSTORE_NAME = getEnvironmentValue("MONGO_PASSWORD_PARAMSTORE_NAME");
-export const MONGO_PASSWORD = getEnvironmentValue("MONGO_PASSWORD");
 export const MONGO_HOST_AND_PORT = getEnvironmentValue("MONGO_HOST_AND_PORT");
 export const MONGO_DB_NAME = getEnvironmentValue("MONGO_DB_NAME");
 export const MONGO_COLLECTION_PROJECTS = getEnvironmentValue("MONGO_COLLECTION_PROJECTS","projects");
